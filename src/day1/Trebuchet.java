672b5fcd43cfd2d19a34fc70cf259b95f627447a
David Lu <davidlu1999@gmail.com>
2023-12-03 00:49:49 +0100

    Added comments

--- src/day1/Trebuchet.java
+++ src/day1/Trebuchet.java
@@ -8,29 +8,39 @@ import java.util.Scanner;
 public class Trebuchet {
     public static void main(String[] args) {
         try {
+            // Create new variables
             File myFile = new File("values.txt");
             Scanner scan = new Scanner(myFile);
-            int sum = 0;
-
             StringBuilder numbersConcat = new StringBuilder();
             ArrayList<String> numbersOnLine = new ArrayList<>();
+            int sum = 0;
+
 
             while (scan.hasNextLine()) {
                 String data = scan.nextLine();
+                // Loop through each character of a line
                 for (int i = 0; i < data.length(); i++) {
                     char letter = data.charAt(i);
+                    // Check if character is 0-9
                     for (int n = 0; n < 10; n++) {
                         if (Character.toString(letter).equals(Integer.toString(n))) {
+                            // Concatenate each number
                             numbersOnLine.add(Integer.toString(n));
                             numbersConcat.append(n);
                         }
                     }
                 }
+                // Reset
                 numbersConcat.setLength(0);
+
+                // Concatenate first and last number
                 numbersConcat.append(numbersOnLine.getFirst());
                 numbersConcat.append(numbersOnLine.getLast());
 
+                // Get sum
                 sum += Integer.parseInt(numbersConcat.toString());
+
+                // Reset
                 numbersOnLine.clear();
             }
             System.out.println(sum);
